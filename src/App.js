28b3c61a1bab045394d71a942ba9455a2f66bb65
Atul Patil <atuluttampatil.1757@gmail.com>
2021-10-27 23:15:49 +0530

    boilerplate react starter and cleanup

--- src/App.js
+++ src/App.js
@@ -3,31 +3,30 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">⚡ Harry Potter ⚡</p>
+          <p className="sub-text">Team up and defeat the Dark Lord</p>
           <div className="connect-wallet-container">
+            
             <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              // src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              src="https://c.tenor.com/pBYo-PJdBMAAAAAC/harry-potter-daniel-radcliffe.gif"
               alt="Monty Python Gif"
             />
           </div>
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
-            href={TWITTER_LINK}
+           
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`built by Atul Patil`}</a>
         </div>
       </div>
     </div>
